427b0d7383a0f93fbfdad96091eb487e0b10a02d
Luis Fernando <74952056+luisfernando42@users.noreply.github.com>
2024-05-27 15:05:21 -0300

    feature: ShopItem created

---
+++ Assets/Scripts/UI/ShopItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ShopItem", menuName = "Shop Item/Item")]
+public class ShopItem : MonoBehaviour
+{
+    public float itemValue;
+    public Sprite itemIcon;
+    public Sprite playerWearing;
+}
