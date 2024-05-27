945c50ca9841cd9f61f8aaec4d1a476311ddf399
Luis Fernando <74952056+luisfernando42@users.noreply.github.com>
2024-05-27 15:06:01 -0300

    feat: Shop Ui template and Shopkeeper

---
+++ Assets/Scripts/Interactables/ShopKeeper.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopKeeper : Interactable
+{
+    public override void Interact()
+    {
+
+    }
+}
